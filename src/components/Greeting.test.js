489e7ed2b0b6e1be8d20c4ba03a9bd0b2e8418d7
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-28 17:02:08 -0500

    testing suite example

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,16 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import Greeting from './Greeting';
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
 
-test('renders Hey Beautiful as a text', () => {
-// Arange
-render(<Greeting/>);
+describe("<Greeting />", () => {
+  // or describe('Greeting component')
+  test("renders Hey Beautiful as a text", () => {
+    // Arange
+    render(<Greeting />);
 
-// Act
-  // ...no function
+    // Act
+    // ...no function
 
-// Assert
-screen.getByText('Hey Beautiful', {exact: false});
-// exact: false = casing won't matter & will match substrings
-const heyBeautifulElement = screen.getByText('Hey Beautiful');
-expect(heyBeautifulElement).toBeInTheDocument();
+    // Assert
+    screen.getByText("Hey Beautiful", { exact: false });
+    // exact: false = casing won't matter & will match substrings
+    const heyBeautifulElement = screen.getByText("Hey Beautiful");
+    expect(heyBeautifulElement).toBeInTheDocument();
+  });
 });
