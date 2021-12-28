f7487b3f9c4d246edb11eb4e450601194ba532de
Celeste Rowe <celeste.rowe@perficient.com>
2021-12-28 17:19:07 -0500

    testing user interaction and states with more test suites

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changeText, setChangeText] = useState(false);
+
+  const changeTextHandler = () => {
+    setChangeText(true);
+  };
+
   return (
     <div>
       <h2>Hey Beautiful</h2>
-      <p>Always great to see your face</p>
+      {!changeText && <p>Always great to see your face</p>}
+      {changeText && <p>Change</p>}
+      <button onClick={changeTextHandler}>Change Text</button>
     </div>
   )
 };
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import Greeting from "./Greeting";
 
 describe("<Greeting />", () => {
@@ -17,3 +18,36 @@ describe("<Greeting />", () => {
     expect(heyBeautifulElement).toBeInTheDocument();
   });
 });
+
+describe("<p>", () => {
+  test("renders good to see you if button is not clicked", () => {
+    render (<Greeting/>)
+    const outputElement = screen.getByText("Always great to see your face", {exact: false});
+    expect(outputElement).toBeInTheDocument();
+  });
+  test('renders changed if button is clicked', () => {
+    // Arrange
+    render (<Greeting/>)
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement)
+
+    // Assert
+    const changeElement = screen.getByText("change text", {exact: false});
+    expect(changeElement).toBeInTheDocument();
+  });
+  test('renders that buton click removes <p>Always great to see your face</p>', () => {
+    // Arrange
+    render(<Greeting/>)
+
+    // Act
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    // Assert 
+    const outputElement = screen.queryByText("great to see your face", {exact: false});
+    expect(outputElement).toBeNull();
+  })
+
+})
